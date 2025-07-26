3a79668372f3550df5a5b88c6312a297154806a0
Oleg <gtdevice@gmail.com>
2025-07-26 11:00:58 +0200

    remove cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from app.api.v1 import api_router
 from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title=settings.project_name,
     openapi_url=f"{settings.api_v1_str}/openapi.json"
+)
+# WARNING: This configuration is insecure and should only be used for testing.
+# It allows requests from any origin.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 # Include API router
